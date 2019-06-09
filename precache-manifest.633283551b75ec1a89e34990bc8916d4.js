d78768da45755005ecc511355bf644a1e5855204
Joohansson <d84@outlook.com>
2019-06-09 19:05:06 +0200

    Updates

---
+++ precache-manifest.633283551b75ec1a89e34990bc8916d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb2e5118f9658f0fd3741c1aca29eac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "093c97ab92c7d8b426c0",
+    "url": "./static/css/main.dab5d9e6.chunk.css"
+  },
+  {
+    "revision": "6683bf63716c6ef412ea",
+    "url": "./static/js/2.beae5ea6.chunk.js"
+  },
+  {
+    "revision": "093c97ab92c7d8b426c0",
+    "url": "./static/js/main.ddac6a8f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
